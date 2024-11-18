e038f6e5cb9de4f6518b21950b2d5769b9f0af48
Zhaoshijun <13165445560@163.com>
2024-11-18 11:26:42 +0800

    增加行情报价

--- service.py
+++ service.py
@@ -1,22 +1,36 @@
+# pip install PyExecJS
+
 import akshare as ak
 from flask import Flask, jsonify, request
 
-stock_individual_info_em_df = ak.stock_individual_info_em(symbol="000001")
-print(stock_individual_info_em_df)
 
 
 
 app = Flask(__name__)
 
 
-# 定义一个 GET 路由
+# 个股信息查询
 @app.route('/api/stock/individual', methods=['GET'])
 def get_stock_individual():
-    return jsonify({"code": 200, "data": stock_individual_info_em_df.to_dict()})
+    symbol = request.args.get('symbol')
+    if symbol is None:
+        return jsonify({"code": 500, "message": "Missing symbol parameter"})
 
+    stock_individual_info_em_df = ak.stock_individual_info_em(symbol=symbol)
+    return jsonify({"code": 200, "data": stock_individual_info_em_df.to_dict(), "message": "success"})
+
+# 行情报价
+@app.route('/api/stock/quote', methods=['GET'])
+def get_stock_quote():
+    symbol = request.args.get('symbol')
+    if symbol is None:
+        return jsonify({"code": 500, "message": "Missing symbol parameter"})
+
+    stock_bid_ask_em_df = ak.stock_bid_ask_em(symbol=symbol)
+    return jsonify({"code": 200, "data": stock_bid_ask_em_df.to_dict(), "message": "success"})
 
 # 启动服务
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000,debug=True)
 
 
