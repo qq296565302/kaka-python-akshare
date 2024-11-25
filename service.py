44fb185d95dfa61b86742b7213d0e0eb33de3cba
Zhaoshijun <13165445560@163.com>
2024-11-25 17:18:19 +0800

    解决DataFrame转JSON格式的问题

--- service.py
+++ service.py
@@ -1,13 +1,10 @@
 # pip install PyExecJS
-
+import pandas as pd
+import json
 import akshare as ak
 from flask import Flask, jsonify, request
 
-
-
-
 app = Flask(__name__)
-
 
 # 个股信息查询
 @app.route('/api/stock/individual', methods=['GET'])
@@ -27,7 +24,17 @@ def get_stock_quote():
         return jsonify({"code": 500, "message": "Missing symbol parameter"})
 
     stock_bid_ask_em_df = ak.stock_bid_ask_em(symbol=symbol)
-    return jsonify({"code": 200, "data": stock_bid_ask_em_df.to_dict(), "message": "success"})
+    return stock_bid_ask_em_df
+
+# 资讯（财联社 cls）
+@app.route('/api/stock/news/cls', methods=['GET'])
+def get_stock_news_cls():
+    symbol = request.args.get('symbol')
+    if symbol is None:
+        return jsonify({"code": 500, "message": "Missing symbol parameter"})
+    stock_info_global_cls_df = ak.stock_info_global_cls(symbol=symbol)
+    result = json.loads(stock_info_global_cls_df.to_json(orient='records', force_ascii=False))
+    return jsonify({"code": 200, "data":result, "message": "success"})
 
 # 启动服务
 if __name__ == '__main__':
